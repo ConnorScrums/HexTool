fac0a8e92a70915260305e69eaa62ae07bb8d0c3
liancent <vincent.liang2400@gmail.com>
2024-10-14 07:00:18 -0400

    wip

--- multiplication_method_hasher.py
+++ multiplication_method_hasher.py
@@ -1,11 +1,22 @@
 from hash import Hash
 from hasher import Hasher
+from hash_64_bit import Hash64Bit
 
 class MultiplicationHasher(Hasher):
     def __init__(self) -> None:
         super().__init__()
+        self.size = 97 # just gonna use the other funcs hash table size
         self.constant = (7 ** 0.5 - 1) / 2
-        self.internal_hashtable = [None for _ in range(self.M_prime)]
+        self.internal_hashtable = [None for _ in range(self.size)]
 
     def hash(self, key) -> Hash:
-        return 0
+        res = Hash64Bit(0)
+
+        product = key * self.constant
+        frac = product * int(product)
+        res = int(self.size * frac)
+
+        for i in bytes:
+            self.internal_hashtable[i] = 0
+
+        return res
