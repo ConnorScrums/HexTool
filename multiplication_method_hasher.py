e200c27c8299b6bc52c4cf26984dcd59981db415
liancent <vincent.liang2400@gmail.com>
2024-10-14 21:34:08 -0400

    need to test function

--- multiplication_method_hasher.py
+++ multiplication_method_hasher.py
@@ -9,14 +9,36 @@ class MultiplicationHasher(Hasher):
         self.constant = (7 ** 0.5 - 1) / 2
         self.internal_hashtable = [None for _ in range(self.size)]
 
-    def hash(self, key) -> Hash:
+    def hash(self, bytes: bytes) -> Hash:
         res = Hash64Bit(0)
 
-        product = key * self.constant
-        frac = product * int(product)
-        res = int(self.size * frac)
-
-        for i in bytes:
-            self.internal_hashtable[i] = 0
-
+        for byte in bytes:
+            product = bytes * self.constant
+            frac = product * int(product)
+            hashed_value = int(self.size * frac)
+            if self.internal_hashtable[hashed_value] is None:
+                self.internal_hashtable[hashed_value] = byte
+                res.item += hashed_value
+            else:
+                i = hashed_value
+                placed = False
+                for i in range(hashed_value, self.size):
+                    if self.internal_hashtable[i] is None:
+                        self.internal_hashtable[i] = byte
+                        placed = True
+                        break
+                if not placed:
+                    for i in range(0, hashed_value):
+                        if self.internal_hashtable[i] is None:
+                            self.internal_hashtable[i] = byte
+                            placed = True
+                            break
+                if not placed:
+                    self.clear_hashtable()
+                    self.internal_hashtable[hashed_value] = byte
+                res.item += hashed_value
         return res
+    
+
+    def clear_hashtable(self):
+        self.internal_hashtable = [None for _ in range(self.size)]
