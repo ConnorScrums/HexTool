8449ae762f5e0aa1acbd45ccb58d26f9c7d1360e
Vincent Liang <vincent.liang2400@gmail.com>
2024-10-14 22:28:30 -0400

    fix math error

--- multiplication_method_hasher.py
+++ multiplication_method_hasher.py
@@ -5,16 +5,17 @@ from hash_64_bit import Hash64Bit
 class MultiplicationHasher(Hasher):
     def __init__(self) -> None:
         super().__init__()
-        self.size = 97 # just gonna use the other funcs hash table size
+        self.size = 73
         self.constant = (7 ** 0.5 - 1) / 2
         self.internal_hashtable = [None for _ in range(self.size)]
 
     def hash(self, bytes: bytes) -> Hash:
+        self.clear_hashtable()
         res = Hash64Bit(0)
 
         for byte in bytes:
-            product = bytes * self.constant
-            frac = product * int(product)
+            product = float(byte) * self.constant
+            frac = product * float(byte) % 1
             hashed_value = int(self.size * frac)
             if self.internal_hashtable[hashed_value] is None:
                 self.internal_hashtable[hashed_value] = byte
