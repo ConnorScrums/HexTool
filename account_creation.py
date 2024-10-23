bb06c264a9ef396ec7ed94aea5ef2580f6aa07fa
Nick Nasta <nnasta3@gmail.com>
2024-10-22 21:01:53 -0400

    Ran linter & cleaned up code

--- account_creation.py
+++ account_creation.py
@@ -1,19 +1,31 @@
+"""
+AccountCreation, connect to DB and create a
+new user account with input from the
+account_creation webpage
+"""
 import pymysql
 
 class AccountCreation():
-    def connectToDataBase(self): 
-        connection = pymysql.connect(host = 'agileteam2db.c922gmyakzhj.us-east-1.rds.amazonaws.com', user = 'admin', 
-                                     password = 'Team2dbpw1!', database = 'HashingApp')
-        connectionCursor = connection.cursor()
-        return connection,connectionCursor
+    """Handle account creation"""
 
-    def createAccount(self, email, password):
-        con,cursor = self.connectToDataBase()
-        print(email, password)
+    def connect_to_database(self):
+        """
+        Connect to the DB, returns the connection & cursor
+        """
+        connection = pymysql.connect(host = 'agileteam2db.c922gmyakzhj.us-east-1.rds.amazonaws.com',
+                                    user = 'admin',password = 'Team2dbpw1!', database = 'HashingApp')
+        connection_cursor = connection.cursor()
+        return connection,connection_cursor
+
+
+    def create_account(self, email, password):
+        """
+        Takes input from webpage and inserts into users table in DB
+        """
+        con,cursor = self.connect_to_database()
 
         sql = "INSERT INTO users (username, password) VALUES (%s, %s)"
         cursor.execute(sql, (email, password))
         con.commit()
 
         con.close
-        
